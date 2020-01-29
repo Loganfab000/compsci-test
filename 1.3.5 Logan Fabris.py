eecdd844d8593d925589c94b0132ce9709519df5
Loganfab000 <60266788+Loganfab000@users.noreply.github.com>
2020-01-29 14:47:56 -0600

    Add files via upload

---
+++ 1.3.5 Logan Fabris.py
@@ -0,0 +1,17 @@
+def how_eligible(paragraph):
+    total = 0
+    if '?' in paragraph:
+            total += 1
+    if '!' in paragraph:
+            total += 1
+    if '"' in paragraph:
+            total += 1 
+    if ',' in paragraph:
+            total += 1     
+    print (total)   
+            
+    
+    
+    
+    
+
